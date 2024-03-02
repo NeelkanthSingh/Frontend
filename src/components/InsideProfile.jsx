904c9286ffabc24d49607f2559b1ad79801af72c
NeelkanthSingh <neelkanthsingh.jr@gmail.com>
2024-03-03 02:18:33 +0530

    Inside Profile done

--- src/components/InsideProfile.jsx
+++ src/components/InsideProfile.jsx
@@ -1,11 +1,59 @@
-import React from 'react'
+import React from "react";
+import Header from "./Header";
+import TitleTextWrapperCard from "./TitleTextWrapperCard";
+import FeatureTextWrapperCard from "./FeatureTextWrapperCard";
+import OverflowWrapperCard from "./OverflowWrapperCard";
+import OptionFeatureWrapperCard from "./OptionFeatureWrapperCard";
 
 const InsideProfile = () => {
   return (
-    <div>
-        
+    <div className=" flex flex-col h-screen">
+      <Header />
+      <div className="flex flex-1 h-full text-center">
+        <div className="w-60 flex flex-col FAF9F6 border-r-2 border-black">
+          <FeatureTextWrapperCard>
+            <div className="">Rename User</div>
+          </FeatureTextWrapperCard>
+          <FeatureTextWrapperCard>
+            <div className="">Delete User</div>
+          </FeatureTextWrapperCard>
+        </div>
+        <div className="flex-1 flex flex-col h-full">
+          <div>
+            <TitleTextWrapperCard>
+              <div className="">Profile</div>
+            </TitleTextWrapperCard>
+          </div>
+          <div className="flex h-full justify-center items-center">
+            <OverflowWrapperCard>
+              <div className="m-12">
+                <div className="m-6">
+                  <OptionFeatureWrapperCard>
+                    <div>Username</div>
+                  </OptionFeatureWrapperCard>
+                </div>
+                <div className="m-6">
+                  <OptionFeatureWrapperCard>
+                    <div>Total Document</div>
+                  </OptionFeatureWrapperCard>
+                </div>
+                <div className="m-6">
+                  <OptionFeatureWrapperCard>
+                    <div>Account Created At</div>
+                  </OptionFeatureWrapperCard>
+                </div>
+                <div className="m-6">
+                  <OptionFeatureWrapperCard>
+                    <div>Total Document Visits</div>
+                  </OptionFeatureWrapperCard>
+                </div>
+              </div>
+            </OverflowWrapperCard>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default InsideProfile
+export default InsideProfile;
