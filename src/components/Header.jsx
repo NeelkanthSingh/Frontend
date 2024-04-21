f5a38ac3de71e40bc33999001646c0ebf4f36627
NeelkanthSingh <neelkanthsingh.jr@gmail.com>
2024-04-21 14:52:43 +0530

    some minor changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,3 @@
-import { Link } from "react-router-dom";
-import LogoHeader from "./Logo/LogoHeader";
-import LogoImage from "./Logo/LogoImage";
-
 const Header = () => {
   return (
     <div className="navbar bg-base-100 text-base-content sticky top-0 z-30 flex h-16 w-full bg-opacity-90 backdrop-blur transition-shadow duration-100 [transform:translate3d(0,0,0)] shadow-sm">
